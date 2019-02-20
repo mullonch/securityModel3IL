c9f6ccb38f02ba39b127411ecbc9039e0a3a4374
Flavien PERIER <flavien.87000@hotmail.fr>
2019-02-20 13:23:28 +0100

    add website base

---
+++ websites/website/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Website presentation</title>
+    <meta charset="utf-8" />
+
+    <link type="text/css" rel="stylesheet" href="node_modules/normalize.css/normalize.css" crossorigin="anonymous" />
+    <link type="text/css" rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css" crossorigin="anonymous" />
+    <link type="text/css" rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css" crossorigin="anonymous" />
+    <link type="text/css" rel="stylesheet" href="css/style.css" crossorigin="anonymous" />
+</head>
+
+<body>
+    <h1>Website presentation</h1>
+
+    <script type="application/javascript" src="node_modules/jquery/dist/jquery.min.js" crossorigin="anonymous"></script>
+    <script type="application/javascript" src="node_modules/jquery-easing/dist/jquery.easing.1.3.umd.min.js" crossorigin="anonymous"></script>
+    <script type="application/javascript" src="node_modules/popper.js/dist/popper.min.js" crossorigin="anonymous"></script>
+    <script type="application/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    
+    <script type="application/javascript" src="js/index.js" crossorigin="anonymous" defer></script>
+    <script type="application/javascript" src="js/genetic.js" crossorigin="anonymous" defer></script>
+</body>
+
+</html>
