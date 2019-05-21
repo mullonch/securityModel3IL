7f3a05975693cef6df3680f7e8678ff65df631a1
Flavien PERIER <flavien.87000@hotmail.fr>
2019-05-21 15:55:07 +0200

    correct buffer overflow server nodejs part

--- Server/buffer-overflow/auth.c
+++ Server/buffer-overflow/auth.c
@@ -4,8 +4,8 @@
 
 void main(int argc, char *argv[]) {
     if (argc == 3 && strcmp(argv[1], "admin") == 0 && strcmp(argv[2], "password") == 0) {
-        printf("1");
+        printf("OK");
     } else {
-        printf("0");
+        printf("KO");
     }
 }
--- Server/buffer-overflow/index.js
+++ Server/buffer-overflow/index.js
@@ -2,21 +2,30 @@
 
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require('body-parser')
+
 const app = express();
 
 const { exec } = require('child_process');
 
+app.use( bodyParser.json() );
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
 
-app.get('/auth', (req, res) => {
-  exec('dir', (err, stdout, stderr) => {
-    if (stdout == 0) {
+app.post('/auth', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  exec(`auth.exe "${username}" "${password}"`, (err, stdout, stderr) => {
+    if (stdout == "OK") {
       ejs.renderFile(__dirname + "/pages/auth.ejs", {}, {}, (err, str) => res.send(str));
     } else {
-  
+      ejs.renderFile(__dirname + "/pages/bad-auth.ejs", {}, {}, (err, str) => res.send(str));
     }
   });
 });
